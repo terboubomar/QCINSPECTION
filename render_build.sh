c250a791e73c3b2d1b7ad6a7d0591c3361990fd8
باست <terboubomar@gmail.com>
2025-08-31 23:10:57 +0300

    Recreate SQLite DB on build to match latest schema

--- render_build.sh
+++ render_build.sh
@@ -1,6 +1,11 @@
-
 #!/usr/bin/env bash
-# Install dependencies
+set -e
+
+# 1) ثبّت المكتبات
 pip install -r requirements.txt
 
+# 2) احذف قاعدة البيانات القديمة إن وُجدت
+rm -f qc_app.db
 
+# 3) أنشئ الجداول وعبّي بيانات أولية
+python -c "from qc_app import app, setup_database; setup_database(app)"
